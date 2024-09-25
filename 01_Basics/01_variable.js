1a725b852f82f97ab7fe087e93a466c7dd5f9141
Naresh Dansena <nareshdansena702@gmail.com>
2024-09-25 09:44:30 +0000

    conversion

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,1 +1,8 @@
-console.log("Naresh");
+const accountId = 1234
+let accountEmail = "sanju@gmail.com"
+var accountPassword = "4321"
+accountCity="CG"
+accountEmail="HGH@gmail.com"
+accountPassword="010101"
+accountCity="BSP"
+console.table([accountEmail,accountId,accountPassword,accountCity])
