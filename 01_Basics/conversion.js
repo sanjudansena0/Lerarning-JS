fb757db9426123dc3064bc85b1fcca0e6d852e58
Naresh Dansena <nareshdansena702@gmail.com>
2024-09-25 11:22:48 +0000

    conversion.js

--- 01_Basics/conversion.js
+++ 01_Basics/conversion.js
@@ -1,9 +1,9 @@
 // -----------------------------Number-String----------------------------
 
-// let someNumber= 33
-// let StringNumber= String(someNumber)
-// console.log(StringNumber);
-// console.log(typeof StringNumber);
+let someNumber= 33
+let StringNumber= String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
 
 
 let score=100
@@ -12,12 +12,14 @@ console.log(Stringscore);
 console.log(typeof Stringscore);
 // ****************************************__ Boolean__*****************************************
 
-// let loggedIn= "naresh"
-// let booleanloggedIn= Boolean(loggedIn)
-// console.log(booleanloggedIn);
-/*
-1= true, 0= false
-""= false, "naresh"= true
+let loggedIn= "naresh"
+let booleanloggedIn= Boolean(loggedIn)         
+console.log(booleanloggedIn);
 
-*/
+// 1 => true, 0 => false 
+//  "" => false, "naresh" => true
+
+
+
+
 
